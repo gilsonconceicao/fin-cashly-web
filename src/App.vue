f2f47b3a793b4e3ec4bf2be8488230ad90a21895
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2025-12-13 21:10:17 -0300

    feat: create router about how test

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
-  
+
 </script>
 
 <template>
   <h1>Hello World</h1>
+
+  <nav>
+    <router-link to="/about">Test router to about</router-link> |
+  </nav>
+
+  <router-view />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/pages/AboutView.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>About view | page</h1>
+</template>
+
+<script lang="ts">
+    
+</script>
