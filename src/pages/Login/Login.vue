67324231878cacedd05c430e64ee03fca93a5ef5
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2026-01-19 21:10:02 -0300

    feat: add vee validate with schema zod

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -2,24 +2,25 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { key_credentials_auth } from '@/constants/localstorage.keys';
-import { reactive } from 'vue';
 import { useRouter } from 'vue-router';
+import { loginInitialValues, loginSchema } from './LoginSchema';
+import { useField, useForm } from 'vee-validate';
 
 const { push } = useRouter();
 
-var form = reactive<{ email: string, password: string }>({
-    email: "",
-    password: ""
+const { handleSubmit } = useForm({
+    validationSchema: loginSchema,
+    initialValues: Object(loginInitialValues)
 })
 
-const handleSubmitForm = () => {
-    if (Object.values(form).some(e => e === null || e.toString()?.length === 0)) {
-        alert("Preencha os campos para seguir em frente")
-    } else {
-        localStorage.setItem(key_credentials_auth, JSON.stringify(form ?? {})); 
-        push('/');
-    }
-}
+const { value: email, errorMessage: emailError } = useField<string>('email')
+const { value: password, errorMessage: passwordError } = useField<string>('password')
+
+const onSubmit = handleSubmit(values => {
+    console.log(values);
+    localStorage.setItem(key_credentials_auth, JSON.stringify(values ?? {}));
+    push('/');
+});
 
 </script>
 
@@ -27,16 +28,18 @@ const handleSubmitForm = () => {
     <div>
         <h1>Login</h1>
 
-        <form @submit.prevent="handleSubmitForm" class="space-y-2 w-400">
+        <form @submit.prevent="onSubmit" class="space-y-2 w-400">
 
             <div class="grid w-full max-w-sm items-center gap-1.5">
                 <Label for="email">Email</Label>
-                <Input id="email" type="email" v-model="form.email" placeholder="Email" />
+                <Input id="email" type="email" v-model="email" placeholder="Email" />
+                <span>{{ emailError }}</span>
             </div>
 
             <div class="grid w-full max-w-sm items-center gap-1.5">
                 <Label for="password">Senha</Label>
-                <Input id="password" type="password" v-model="form.password" placeholder="Senha" />
+                <Input id="password" type="password" v-model="password" placeholder="Senha" />
+                <span>{{ passwordError }}</span>
             </div>
 
             <Button type="submit">
