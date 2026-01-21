088e41a5c9c680a164158377dad0aa1857fbc58f
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2026-01-20 21:38:11 -0300

    feat: create generic controlled form

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -1,23 +1,17 @@
 <script lang="ts" setup>
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
 import { key_credentials_auth } from '@/constants/localstorage.keys';
 import { useRouter } from 'vue-router';
 import { loginInitialValues, loginSchema } from './LoginSchema';
-import { useField, useForm } from 'vee-validate';
-import InputFormField from '@/components/forms/InputFormField.vue';
+import InputFormField from '@/components/forms/Fields/InputFormField.vue';
+import ControlledForm from '@/components/forms/ControlledForm.vue';
 
 const { push } = useRouter();
 
-const { handleSubmit } = useForm({
-    validationSchema: loginSchema,
-    initialValues: Object(loginInitialValues)
-});
-
-const onSubmit = handleSubmit(values => {
+const onSubmit = (values: Object) => {
     localStorage.setItem(key_credentials_auth, JSON.stringify(values ?? {}));
     push('/');
-});
+};
 
 </script>
 
@@ -25,12 +19,12 @@ const onSubmit = handleSubmit(values => {
     <div>
         <h1>Login</h1>
 
-        <form @submit.prevent="onSubmit" class="space-y-2 w-400">
+        <ControlledForm  :schema="loginSchema" :initialvalues="loginInitialValues" :onsubmit="onSubmit">
             <InputFormField label="E-mail" name="email" placeholder="E-mail" :required="true" type="email"/>
             <InputFormField label="Senha" name="password" placeholder="Senha" :required="true" type="password"/>
             <Button type="submit">
                 Acessar
             </Button>
-        </form>
+        </ControlledForm>
     </div>
 </template>
